800535e6aac15324d868b0b6d16d38751a499d7c
Tomislav Dukez <tomdu3@ymail.com>
2025-08-31 21:00:34 +0100

    add products example

--- models.py
+++ models.py
@@ -7,3 +7,17 @@ class Product(BaseModel):
     description: str
     price: float
     quantity: int
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": 5,
+                    "name": "airpods",
+                    "description": "wireless earphones",
+                    "price": 100,
+                    "quantity": 1,
+                }
+            ]
+        }
+    }
